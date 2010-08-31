b7462208e7a1391a4c8cf449b2c7de3affab2fd9
garaemon <garaemon@gmail.com>
2010-08-31 09:34:34 +0900

    first commit

---
+++ src/osg/Vec2.cpp
@@ -0,0 +1,37 @@
+#include <osg/Vec2>
+
+extern "C"
+{
+    // allocator
+    osg::Vec2* osg_Vec2_new()
+    {
+        return new osg::Vec2();
+    }
+    
+    // disposer
+    void osg_Vec2_dispose(osg::Vec2* ptr)
+    {
+        free ptr;
+    }
+    
+    // setter
+    void osg_Vec2_set_x(osg::Vec2* ptr, float x)
+    {
+        ptr->_v[0] = x;
+    }
+    void osg_Vec2_set_y(osg::Vec2* ptr, float y)
+    {
+        ptr->_v[1] = y;
+    }
+    
+    // getter
+    double osg_Vec2_get_x(osg::Vec2* ptr)
+    {
+        return ptr->_v[0];
+    }
+    double osg_Vec2_get_y(osg::Vec2* ptr)
+    {
+        return ptr->_v[1];
+    }
+}
+
